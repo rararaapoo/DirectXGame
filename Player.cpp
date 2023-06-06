f424724fb4d90d8f6f1cc6d64637c5e78353d3f5
user <user@DESKTOP-PQHUCC1>
2023-06-06 12:31:36 +0900

    test01

--- Player.cpp
+++ Player.cpp
@@ -12,7 +12,24 @@ void Player::Initialize(Model* model, uint32_t textureHandle)
 
 }
 
-void Player::Update() { textureHandle_; }
+	if (input_->PushKey(DIK_UP)) 
+	{
+		move.y -= kCharacterSpeed;
+	}
+
+	else if (input_->PushKey(DIK_RIGHT)) 
+	{
+		move.y += kCharacterSpeed;
+	}
+	
+	worldTransform_.translation_.x += move.x;
+	worldTransform_.translation_.y += move.y;
+	worldTransform_.translation_.z += move.z;
+	
+	worldTransform_.matWorld_ = MakeAffineMetrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+	
+}
 
 void Player::Draw(ViewProjection viewProjection_) {
 	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
